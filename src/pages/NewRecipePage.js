74143bd03c7af091de162386568d3351e71ced37
Hadi Ghazi <hadigghazi@gmail.com>
2024-07-21 09:32:21 +0300

    Adding the new recipe page

--- src/pages/NewRecipePage.js
+++ src/pages/NewRecipePage.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const NewRecipePage = () => {
-  return (
-    <div>NewRecipePage</div>
-  )
-}
+  const [name, setName] = useState('');
+  const [ingredients, setIngredients] = useState('');
+  const [steps, setSteps] = useState('');
 
-export default NewRecipePage
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newRecipe = {
+      name,
+      ingredients: ingredients.split(','),
+      steps: steps.split(',')
+    };
+
+    axios.post('/create_recipe.php', newRecipe)
+      .then(response => {
+        console.log("Recipe created successfully!");
+      })
+      .catch(error => {
+        console.error("There was an error creating the recipe!", error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Create New Recipe</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div>
+          <label>Ingredients (comma separated):</label>
+          <input type="text" value={ingredients} onChange={(e) => setIngredients(e.target.value)} />
+        </div>
+        <div>
+          <label>Steps (comma separated):</label>
+          <input type="text" value={steps} onChange={(e) => setSteps(e.target.value)} />
+        </div>
+        <button type="submit">Create Recipe</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewRecipePage;
