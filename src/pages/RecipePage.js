ec0cfdc77856d07179dde2f0d68e2417ef8e38d7
Hadi Ghazi <hadigghazi@gmail.com>
2024-07-21 09:31:32 +0300

    Adding the recipe page

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams, Link } from 'react-router-dom';
 
 const RecipePage = () => {
-  return (
-    <div>RecipePage</div>
-  )
-}
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState(null);
 
-export default RecipePage
+  useEffect(() => {
+    axios.get(`/get_recipe.php?id=${id}`)
+      .then(response => {
+        setRecipe(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the recipe!", error);
+      });
+  }, [id]);
+
+  if (!recipe) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{recipe.name}</h1>
+      <h2>Ingredients</h2>
+      <ul>
+        {recipe.ingredients.split(',').map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <h2>Steps</h2>
+      <ol>
+        {recipe.steps.split(',').map((step, index) => (
+          <li key={index}>{step}</li>
+        ))}
+      </ol>
+      <Link to={`/update/${id}`}>Edit Recipe</Link>
+    </div>
+  );
+};
+
+export default RecipePage;
