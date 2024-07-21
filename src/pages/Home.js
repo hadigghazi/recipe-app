73e359e9a7c4c27d890b4bea741ae5838ae3e859
Hadi Ghazi <hadigghazi@gmail.com>
2024-07-21 09:30:57 +0300

    Adding the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [recipes, setRecipes] = useState([]);
 
-export default Home
+  useEffect(() => {
+    axios.get('/get_recipes.php')
+      .then(response => {
+        setRecipes(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the recipes!", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Recipe List</h1>
+      <ul>
+        {recipes.map(recipe => (
+          <li key={recipe.id}>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
